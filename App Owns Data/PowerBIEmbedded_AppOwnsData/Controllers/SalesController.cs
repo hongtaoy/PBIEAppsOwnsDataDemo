26898d19b4e48eee4cffabd64e0e8a0547139453
Hongtao Yang <32990694+hongtaoy@users.noreply.github.com>
2019-10-29 18:26:02 -0500

    initial commit

---
+++ App Owns Data/PowerBIEmbedded_AppOwnsData/Controllers/SalesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PowerBIEmbedded_AppOwnsData.Models;
+using System.Diagnostics;
+
+namespace PowerBIEmbedded_AppOwnsData.Controllers
+{
+    public class SalesController : Controller
+    {
+        private SalesDBContext db = new SalesDBContext();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            Debug.WriteLine("loading index");
+            Console.WriteLine("loding index console");
+            return View(db.Sales.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        [HttpPost]
+        public Boolean postData(Sale row)
+        {
+            Debug.WriteLine("json received");
+            Debug.WriteLine(row.State);
+            db.Sales.Add(row);
+            db.SaveChanges();
+            return true;
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,State,Sales,Quantity,Unit_Price")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sales.Add(sale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sale);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sale);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sales.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sale sale = db.Sales.Find(id);
+            db.Sales.Remove(sale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ App Owns Data/PowerBIEmbedded_AppOwnsData/Models/Sales.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class Sale
+    {
+        public int ID { get; set; }
+        public string State { get; set; }
+        public string Sales { get; set; }
+        public int Quantity { get; set; }
+        public string Unit_Price { get; set; }
+    }
+
+    public class SalesDBContext : DbContext
+    {
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
